0c172d5e5eab601d290d3ddb443ccaa0e975eeee
kearney <mike.wayne.kearney@gmail.com>
2021-11-05 11:23:20 -0500

    simplify

--- R/bar.R
+++ R/bar.R
@@ -9,45 +9,45 @@
 #' @param max_width Desired max width
 #' @return A progress bar list with tick and done functions
 #' @export
-pbr <- function(iterations, width = NULL, min_width = 10, max_width = 100L) {
-  ## determine width
-  width <- get_width(width, min_width, max_width)
-
-  ## create progress bar environment
-  .pb <- new.env(hash = FALSE, parent = environment())
-
-  ## initialize values in .pb
-  assign("i", 0L, envir = .pb)
-  assign("N", iterations, envir = .pb)
-  assign("width", width, envir = .pb)
-  assign("init", as.numeric(Sys.time()) - 1, envir = .pb)
-
+pbr <- function(iterations, width = NULL, min_width = 10, max_width = 1000L) {
+  ## create progress bar environment and initialize values
+  .pb <- new.env(
+    hash = FALSE,
+    parent = as.environment(
+      list(
+        i = 0L,
+        width = get_width(width, min_width, max_width),
+        N = iterations,
+        init = as.integer(Sys.time() - 0L)
+      )
+    )
+  )
   ## construct list
   pb <- list(
     tick = function() {
       N <- get("N", envir = .pb)
-      i <- (get("i", envir = .pb) + 1L) %>>% N
+      i <- get("i", envir = .pb) + 1L %>>% N
       assign("i", i, envir = .pb)
-      secs_elp <- as.numeric(Sys.time()) - get("init", envir = .pb)
-      print(pb_build_msg(
+      secs_elp <- as.integer(Sys.time()) - get("init", envir = .pb)
+      cat(pb_build_msg(
         prop = i / N,
         width = get("width", envir = .pb),
-        secs = as.integer(secs_elp / (i %<<% 2) * N - secs_elp)
+        secs = as.integer(((secs_elp / i) %>>% 1) * N - secs_elp)
       ))
     },
     done = function() {
       cat(pb_build_msg(
         prop = 1,
         width = get("width", envir = .pb),
-        secs =
+        secs = NULL
       ))
-      cat(.makeMessage(paste0("\r", hspaces(getOption("width")), "\r")))
+      cat(paste0("\r", hspaces(getOption("width")), "\r"))
     }
   )
 
   ## initialize progress bar
-  print(pb_build_msg(prop = 0, width = width))
+  cat(pb_build_msg(prop = 0, width = width))
 
   ## return pbr object
-  as_pbr(pb)
+  pb
 }
--- R/message.R
+++ R/message.R
@@ -1,35 +1,51 @@
 
 ## add print colors to text
 pdone <- function(x) {
-    paste0("\033[38;5;155m", x, "\033[39m")
+  if (x == "") {
+    return(x)
+  }
+  paste0("\033[38;5;155m", x, "\033[39m")
 }
 prem <- function(x) {
-    paste0("\033[38;5;244m", x, "\033[39m")
+  if (x == "") {
+    return(x)
+  }
+  paste0("\033[38;5;242m", x, "\033[39m")
+}
+pnew <- function(x) {
+  if (x == "") {
+    return(x)
+  }
+  paste0("\033[38;5;255m", x, "\033[39m")
 }
 
 ## quick functions for horizontal spaces and bars
-hbar0 <- function() "\u2588"
+hbar0 <- function() "\u2592"
 hbar1 <- function() "\u2588"
+hbarr <- function() "\u2593"
+
 hbars0 <- function(n) if (n > 0) paste(rep(hbar0(), n), collapse = "") else ""
 hbars1 <- function(n) if (n > 0) paste(rep(hbar1(), n), collapse = "") else ""
+hbarsr <- function(n) if (n > 0) paste(rep(hbarr(), n), collapse = "") else ""
+
 hspace <- function() " "
 hspaces <- function(n) if (n > 0) paste(rep(hspace(), n), collapse = "") else ""
 
 ## format elements
 format_secs <- function(secs = NULL) {
   if (is.null(secs)) {
-    return("     ")
+    return("      ")
   }
   secs <- if (secs > 129599) {
-    sprintf(" %3dd", as.integer(secs / 86400))
+    sprintf(" %3dd ", as.integer(secs / 86400))
   } else if (secs > 3599) {
-    sprintf(" %3dh", as.integer(secs / 3600))
+    sprintf(" %3dh ", as.integer(secs / 3600))
   } else if (secs > 90) {
-    sprintf(" %3dm", as.integer(secs / 60))
+    sprintf(" %3dm ", as.integer(secs / 60))
   } else if (secs > 0) {
-    sprintf(" %3ds", as.integer(secs))
+    sprintf(" %3ds ", as.integer(secs))
   } else {
-    ""
+    "   0s "
   }
   pdone(secs)
 }
@@ -37,39 +53,24 @@ format_secs <- function(secs = NULL) {
 format_prop <- function(prop, width) {
   done <- round(prop * width)
   left <- width - done
-  sprintf("%s%s", pdone(hbars1(done)), prem(hbars0(left)))
+  sprintf("%s%s%s",
+    pdone(hbars1(done - 1)),
+    pnew(hbarsr(as.integer(done - 1 > 0))),
+    prem(hbars0(left)))
 }
 
 ## adjust width given width of secs
-calc_width <- function(width = NULL, secs = 5L) {
+calc_width <- function(width = NULL, secs = 6L) {
   if (is.null(width)) {
     width <- getOption("width", 80)
   }
   width - secs
 }
 
-as_pbr_msg <- function(x) structure(x, class = "pbr_msg")
-as_pbr <- function(x) structure(x, class = c("pbr", "list"))
-
-print.pbr_msg <- function(x, ...) {
-  cat(.makeMessage(x))
-  cat(.makeMessage("\r"))
-}
-
-#' @export
-print.pbr <- function(x, ...) {
-  cat(sprintf(
-    "# A pbr progress bar (N = %d)", x$iterations), fill = TRUE)
-  cat("$tick() = iterate\n$done() = complete\n")
-}
-
-
 ## build the actual bar
 pb_build_msg <- function(prop, width = NULL, secs = NULL) {
-  as_pbr_msg(
-    paste0(
-      format_prop(prop, calc_width(width)),
-      format_secs(secs)
-    )
-  )
+  paste0("\r",
+    format_prop(prop, calc_width(width)),
+    format_secs(secs),
+    "")
 }
